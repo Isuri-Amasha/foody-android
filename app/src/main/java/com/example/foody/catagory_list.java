420f40e0ce633409f3938faca1535569f215d89d
Sajani97 <sajani.udara9742@gmail.com>
2020-08-16 22:25:05 +0530

    Catagery Management

---
+++ app/src/main/java/com/example/foody/catagory_list.java
@@ -0,0 +1,14 @@
+package com.example.foody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class catagory_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catagory_list);
+    }
+}
